2f65482f9a0f9730c30a6a5a6f10d0bb6e37a4ab
lmwis <detectiveygn@163.com>
2020-10-21 20:21:58 +0800

    feat<A>:accept

--- algorithm-experiment/src/FibonacciSequence.java
+++ algorithm-experiment/src/FibonacciSequence.java
@@ -1,4 +1,4 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 /**
  * @Description:
@@ -7,4 +7,22 @@ package PACKAGE_NAME;
  * @Version 1.0
  */
 public class FibonacciSequence {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = scanner.nextInt();
+        System.out.println(dep(n));
+    }
+
+    public static long dep(int  n){
+        if(n==1){
+            return 1;
+        }else if(n==2){
+            return 2;
+        }else if(n==3){
+            return 3;
+        }else{
+            return dep(n-1)+dep(n-2)+dep(n-3);
+        }
+    }
 }
