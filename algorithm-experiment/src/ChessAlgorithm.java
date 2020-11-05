87de713196b606761a73e54c8b9f05fc23652a9b
lmwis <detectiveygn@163.com>
2020-11-05 20:49:22 +0800

    feat<alg>:调优步骤

---
+++ algorithm-experiment/src/ChessAlgorithm.java
@@ -0,0 +1,16 @@
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @Description:
+ * @Author: lmwis
+ * @Date 2020-11-04 19:16
+ * @Version 1.0
+ */
+public class ChessAlgorithm {
+
+    public static void main(String[] args) {
+        final Map<Object, Object> objectObjectMap = Collections.synchronizedMap(new HashMap<>());
+    }
+}
