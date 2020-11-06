430178e0d3dbf6ff7ab7e26e979ac95fa1e41cfe
lmwis <detectiveygn@163.com>
2020-11-06 16:30:27 +0800

    feat<redis_demo>:init

---
+++ redis-demo/src/main/java/com/lmwis/interview/redis/RedisDemoApplication.java
@@ -0,0 +1,17 @@
+package com.lmwis.interview.redis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Description:
+ * @Author: lmwis
+ * @Date 2020-11-06 16:29
+ * @Version 1.0
+ */
+@SpringBootApplication
+public class RedisDemoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RedisDemoApplication.class,args);
+    }
+}
