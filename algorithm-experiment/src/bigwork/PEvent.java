8eda42a17516f6e595e30eee721bc998852024ca
lmwis <detectiveygn@163.com>
2020-11-08 16:35:34 +0800

    feat<big work>:one finish

--- algorithm-experiment/src/bigwork/PEvent.java
+++ algorithm-experiment/src/bigwork/PEvent.java
@@ -1,4 +1,99 @@
 package bigwork;
 
+import java.util.Objects;
+
 public class PEvent {
+    private Double x;
+    private Double y;
+    // 时间戳
+    private long time;
+
+    public PEvent(Double x, Double y, long time) {
+        this.x = x;
+        this.y = y;
+        this.time = time;
+    }
+
+    public String getDesigner(){
+        // 补充姓名学号
+        return "设计者：[姓名] 学号：[学号] 班级：[班级]";
+    }
+
+    /**
+     * 直角坐标系转极坐标系
+     * @return
+     */
+    public REvent convertToREvent(){
+        double r = Math.sqrt(x*x+y*y);
+        double angle = Math.asin(y/r);
+        return new REvent(r,angle,time);
+    }
+
+    /**
+     * 到原点的距离
+     * @return
+     */
+    public double distance(){
+        return Math.sqrt(x*x+y*y);
+    }
+
+    /**
+     * 两点间的距离
+     * @param pevent
+     * @return
+     * @throws Exception
+     */
+    public double distance(PEvent pevent) throws Exception {
+        if (time!=pevent.getTime())
+            throw new Exception("不为同一时刻没有意义");
+        return Math.sqrt(Math.abs((x-pevent.x)*(x-pevent.x)+(y-pevent.y)*(y-pevent.y)));
+    }
+
+    @Override
+    public String toString() {
+        return "PEvent{" +
+                "x=" + x +
+                ", y=" + y +
+                ", time=" + time +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        PEvent pEvent = (PEvent) o;
+        return time == pEvent.time &&
+                Objects.equals(x, pEvent.x) &&
+                Objects.equals(y, pEvent.y);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(x, y, time);
+    }
+
+    public Double getX() {
+        return x;
+    }
+
+    public void setX(Double x) {
+        this.x = x;
+    }
+
+    public Double getY() {
+        return y;
+    }
+
+    public void setY(Double y) {
+        this.y = y;
+    }
+
+    public long getTime() {
+        return time;
+    }
+
+    public void setTime(long time) {
+        this.time = time;
+    }
 }
--- algorithm-experiment/src/bigwork/REvent.java
+++ algorithm-experiment/src/bigwork/REvent.java
@@ -1,4 +1,99 @@
 package bigwork;
 
+import java.util.Objects;
+
 public class REvent {
+    private Double r;
+    private Double angle;
+    // 时间戳
+    private long time;
+
+    public REvent(Double r, Double angle, long time) {
+        this.r = r;
+        this.angle = angle;
+        this.time = time;
+    }
+
+    public String getDesigner(){
+        // 补充姓名学号
+        return "设计者：[姓名] 学号：[学号] 班级：[班级]";
+    }
+
+    /**
+     * 极坐标系转直角坐标系
+     * @return
+     */
+    public PEvent convertToPEvent(){
+        double x = r*Math.cos(angle);
+        double y = r*Math.sin(angle);
+        return new PEvent(x,y,time);
+    }
+
+    /**
+     * 到远点的距离
+     * @return
+     */
+    public double distance(){
+        return r;
+    }
+
+    /**
+     * 两点间的距离
+     * @param revent
+     * @return
+     * @throws Exception
+     */
+    public double distance(REvent revent) throws Exception {
+        if (time!=revent.getTime())
+            throw new Exception("不为同一时刻没有意义");
+        return Math.sqrt(r*r+revent.getR()*revent.getR()-2*r*revent.getR()*Math.cos(angle-revent.getAngle()));
+    }
+
+    @Override
+    public String toString() {
+        return "REvent{" +
+                "r=" + r +
+                ", angle=" + angle +
+                ", time=" + time +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        REvent rEvent = (REvent) o;
+        return time == rEvent.time &&
+                Objects.equals(r, rEvent.r) &&
+                Objects.equals(angle, rEvent.angle);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(r, angle, time);
+    }
+
+    public Double getR() {
+        return r;
+    }
+
+    public void setR(Double r) {
+        this.r = r;
+    }
+
+    public Double getAngle() {
+        return angle;
+    }
+
+    public void setAngle(Double angle) {
+        this.angle = angle;
+    }
+
+    public long getTime() {
+        return time;
+    }
+
+    public void setTime(long time) {
+        this.time = time;
+    }
 }
