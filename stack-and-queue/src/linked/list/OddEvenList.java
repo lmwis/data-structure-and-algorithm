c397ecc0bee35a8897e8806e386d5a7e6113f2a8
lmwis <detectiveygn@163.com>
2020-11-13 10:54:51 +0800

    feat<days code>:accept

---
+++ stack-and-queue/src/linked/list/OddEvenList.java
@@ -0,0 +1,41 @@
+package linked.list;
+
+/**
+ * @Description: 328. 奇偶链表
+ * 给定一个单链表，把所有的奇数节点和偶数节点分别排在一起。请注意，这里的奇数节点和偶数节点指的是节点编号的奇偶性，而不是节点的值的奇偶性。
+ * 请尝试使用原地算法完成。你的算法的空间复杂度应为 O(1)，时间复杂度应为 O(nodes)，nodes 为节点总数。
+ * 示例 1:
+ *
+ * 输入: 1->2->3->4->5->NULL
+ * 输出: 1->3->5->2->4->NULL
+ * @Author: lmwis
+ * @Date 2020-11-13 10:53
+ * @Version 1.0
+ */
+public class OddEvenList {
+    public static void main(String[] args) {
+
+    }
+
+    /**
+     * 分离节点后合并
+     * @param head
+     * @return
+     */
+    public ListNode oddEvenList(ListNode head) {
+        if (head == null) {
+            return head;
+        }
+        ListNode evenHead = head.next;
+        ListNode odd = head, even = evenHead;
+        while (even != null && even.next != null) {
+            odd.next = even.next;
+            odd = odd.next;
+            even.next = odd.next;
+            even = even.next;
+        }
+        odd.next = evenHead;
+        return head;
+    }
+
+}
