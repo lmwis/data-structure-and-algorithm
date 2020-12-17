0505260d10b9a06f707f9a4cdc847896e3fca0bc
lmwis <detectiveygn@163.com>
2020-12-17 21:44:01 +0800

    feat<thread local>: thread local demo

---
+++ Interview/src/concurrent/volatiledemo/ThreadLocalDemo.java
@@ -0,0 +1,13 @@
+package concurrent.volatiledemo;
+
+/**
+ * @Description:
+ * @Author: lmwis
+ * @Date 2020-12-16 21:16
+ * @Version 1.0
+ */
+public class ThreadLocalDemo {
+    public static void main(String[] args) {
+        ThreadLocal thread = new ThreadLocal();
+    }
+}
