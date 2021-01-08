b00f3ba8c469198976c92e2c7d3ccd56a2b0c131
lmwis <detectiveygn@163.com>
2021-01-08 19:08:54 +0800

    feat<days code>: accept

--- stack-and-queue/src/array/medium/RotateArray.java
+++ stack-and-queue/src/array/medium/RotateArray.java
@@ -37,4 +37,31 @@ public class RotateArray {
             pre1 = current;
         }
     }
+
+
+    /**
+     * 环形替换
+     * @param nums
+     * @param k
+     */
+    public void rotateBest(int[] nums, int k) {
+        int n = nums.length;
+        k = k % n;
+        int count = gcd(k, n);
+        for (int start = 0; start < count; ++start) {
+            int current = start;
+            int prev = nums[start];
+            do {
+                int next = (current + k) % n;
+                int temp = nums[next];
+                nums[next] = prev;
+                prev = temp;
+                current = next;
+            } while (start != current);
+        }
+    }
+
+    public int gcd(int x, int y) {
+        return y > 0 ? gcd(y, x % y) : x;
+    }
 }
