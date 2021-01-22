e97f3dacb21f71f3c6846a05269c9322fa1cb18e
lmwis <detectiveygn@163.com>
2021-01-22 23:27:23 +0800

    feat<days code>: accept

---
+++ stack-and-queue/src/easy/AddToArrayForm.java
@@ -0,0 +1,45 @@
+package easy;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * @Description: 989. 数组形式的整数加法
+ * 对于非负整数 X 而言，X 的数组形式是每位数字按从左到右的顺序形成的数组。例如，如果 X = 1231，那么其数组形式为 [1,2,3,1]。
+ * 给定非负整数 X 的数组形式 A，返回整数 X+K 的数组形式。
+ *
+ * 示例 1：
+ *
+ * 输入：A = [1,2,0,0], K = 34
+ * 输出：[1,2,3,4]
+ * 解释：1200 + 34 = 1234
+ * 示例 2：
+ *
+ * 输入：A = [2,7,4], K = 181
+ * 输出：[4,5,5]
+ * 解释：274 + 181 = 455
+ * @Author: lmwis
+ * @Date 2021-01-22 23:26
+ * @Version 1.0
+ */
+public class AddToArrayForm {
+    public List<Integer> addToArrayForm(int[] A, int K) {
+        List<Integer> res = new ArrayList<Integer>();
+        int n = A.length;
+        for (int i = n - 1; i >= 0; --i) {
+            int sum = A[i] + K % 10;
+            K /= 10;
+            if (sum >= 10) {
+                K++;
+                sum -= 10;
+            }
+            res.add(sum);
+        }
+        for (; K > 0; K /= 10) {
+            res.add(K % 10);
+        }
+        Collections.reverse(res);
+        return res;
+    }
+}
