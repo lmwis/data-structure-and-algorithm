5b177a4eefca8d1a6ce1f6de2ba14eee7cc796df
lmwis <detectiveygn@163.com>
2021-03-04 23:16:49 +0800

    feat<sort>: shell sort method unfinish

---
+++ stack-and-queue/src/sort/ShellSort.java
@@ -0,0 +1,19 @@
+package sort;
+
+import java.util.Arrays;
+
+/**
+ * @Description: 希尔排序  优化过的插入排序
+ * @Author: lmwis
+ * @Date 2021-03-04 23:15
+ * @Version 1.0
+ */
+public class ShellSort {
+    public static void main(String[] args) {
+        int[] array = new int[]{8,4,7,2,4,7};
+        System.out.println(Arrays.toString(array));
+
+        int grep = array.length/2;
+
+    }
+}
