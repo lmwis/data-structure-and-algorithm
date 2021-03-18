ead8e56838c1ee2179a881194c89232694264f9f
lmwis <detectiveygn@163.com>
2021-03-18 11:28:09 +0800

    feat<concurrent>: Semaphore demo

---
+++ Interview/src/concurrent/volatiledemo/concurrent/SemaphoreDemo.java
@@ -0,0 +1,10 @@
+package concurrent.volatiledemo.concurrent;
+
+/**
+ * @Description: 信号量，类似java中的锁
+ * @Author: lmwis
+ * @Date 2021-03-18 11:05
+ * @Version 1.0
+ */
+public class SemaphoreDemo {
+}
