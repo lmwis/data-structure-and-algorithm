b3e9217be91e01f1afcb8acde09a4d38130031af
lmwis <detectiveygn@163.com>
2021-04-05 16:13:40 +0800

    feat<leetcode>: days code

---
+++ stack-and-queue/src/array/easy/Merge.java
@@ -0,0 +1,32 @@
+package array.easy;
+
+/**
+ * @Description:
+ * @Author: lmwis
+ * @Date 2021-04-05 15:55
+ * @Version 1.0
+ */
+public class Merge {
+    public static void main(String[] args) {
+
+    }
+
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int p1 = m - 1, p2 = n - 1;
+        int tail = m + n - 1;
+        int cur;
+        while (p1 >= 0 || p2 >= 0) {
+            if (p1 == -1) {
+                cur = nums2[p2--];
+            } else if (p2 == -1) {
+                cur = nums1[p1--];
+            } else if (nums1[p1] > nums2[p2]) {
+                cur = nums1[p1--];
+            } else {
+                cur = nums2[p2--];
+            }
+            nums1[tail--] = cur;
+        }
+    }
+
+}
