923cbce2eeebdd0c4bc59521f5c38c28e3033698
lmwis <detectiveygn@163.com>
2021-04-10 20:20:48 +0800

    feat<leetcode>: 京东笔试

---
+++ demo/src/jindong/Q1.java
@@ -0,0 +1,16 @@
+package jindong;
+
+import java.util.Scanner;
+
+/**
+ * @Description:
+ * @Author: lmwis
+ * @Date 2021-04-10 19:21
+ * @Version 1.0
+ */
+public class Q1 {
+    public static void main(String[] args) {
+        Scanner input  = new Scanner(System.in);
+        int n = input.nextInt();
+    }
+}
