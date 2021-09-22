b442fa9acdf6a06d3dd1942680c9a789b6b52b9d
lmwis <detectiveygn@163.com>
2021-09-22 09:27:26 +0800

    sword-finger:days code

---
+++ stack-and-queue/src/dfs/MaxAreaOfIsland.java
@@ -0,0 +1,64 @@
+package dfs;
+
+/**
+ * @Description: 695. 岛屿的最大面积
+ * 给定一个包含了一些 0 和 1 的非空二维数组 grid 。
+ *
+ * 一个 岛屿 是由一些相邻的 1 (代表土地) 构成的组合，这里的「相邻」要求两个 1 必须在水平或者竖直方向上相邻。你可以假设 grid 的四个边缘都被 0（代表水）包围着。
+ *
+ * 找到给定的二维数组中最大的岛屿面积。(如果没有岛屿，则返回面积为 0 。)
+ *
+ * 示例 1:
+ *
+ * [[0,0,1,0,0,0,0,1,0,0,0,0,0],
+ *  [0,0,0,0,0,0,0,1,1,1,0,0,0],
+ *  [0,1,1,0,1,0,0,0,0,0,0,0,0],
+ *  [0,1,0,0,1,1,0,0,1,0,1,0,0],
+ *  [0,1,0,0,1,1,0,0,1,1,1,0,0],
+ *  [0,0,0,0,0,0,0,0,0,0,1,0,0],
+ *  [0,0,0,0,0,0,0,1,1,1,0,0,0],
+ *  [0,0,0,0,0,0,0,1,1,0,0,0,0]]
+ * 对于上面这个给定矩阵应返回 6。注意答案不应该是 11 ，因为岛屿只能包含水平或垂直的四个方向的 1 。
+ *
+ * 示例 2:
+ *
+ * [[0,0,0,0,0,0,0,0]]
+ * 对于上面这个给定的矩阵, 返回 0。
+ *
+ * @Author: lmwis
+ * @Data: 2021/9/19 4:58 下午
+ * @Version: 1.0
+ */
+public class MaxAreaOfIsland {
+
+    int max = 0;
+    int count = 0;
+    public int maxAreaOfIsland(int[][] grid) {
+        for(int i=0;i<grid.length;i++){
+            for(int j=0;j<grid[0].length;j++){
+                if(grid[i][j]==1){
+                    dfs(grid,i,j);
+                    max = Math.max(max,count);
+                    count=0;
+                }
+            }
+        }
+        return max;
+
+    }
+    public void dfs(int[][] grid,int x,int y){
+        if(x<0||y<0||x>=grid.length||y>=grid[0].length){
+            return ;
+        }
+        if(grid[x][y]!=1){
+            return;
+        }
+        grid[x][y]=2;
+        this.count++;
+        dfs(grid,x+1,y);
+        dfs(grid,x-1,y);
+        dfs(grid,x,y+1);
+        dfs(grid,x,y-1);
+    }
+
+}
