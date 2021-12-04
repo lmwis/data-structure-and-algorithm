0b9afe75c1d8633bc22b784b148454b549b767a7
lmwis <detectiveygn@163.com>
2021-12-04 19:33:51 +0800

    add feature Embarket

--- sword-finger-offer/src/easy/TwoSum.java
+++ sword-finger-offer/src/easy/TwoSum.java
@@ -1,5 +1,7 @@
 package easy;
 
+import java.util.Scanner;
+
 /**
  * @Description: 剑指 Offer 57. 和为s的两个数字
  * 输入一个递增排序的数组和一个数字s，在数组中查找两个数，使得它们的和正好是s。如果有多对数字的和等于s，则输出任意一对即可。
@@ -18,6 +20,28 @@ package easy;
  * @Version: 1.0
  */
 public class TwoSum {
+
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        String str ="";
+        while(in.hasNext()){
+            str =str + in.nextLine();
+        }
+
+        int index = 0;
+        int count = 0;
+        while(true){
+            index = str.indexOf("SUST");
+            if (index<0){
+                break;
+            }else{
+                count++;
+            }
+            str = str.substring(index+4);
+        }
+        System.out.println(count);
+    }
 
     public int[] twoSum(int[] nums, int target) {
         int[] res = new int[2];
