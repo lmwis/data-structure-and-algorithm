f828ff44e2e6319296cc90bc0bb469fde25fe982
lmwis <detectiveygn@163.com>
2021-12-22 15:50:02 +0800

    feat:so rt

--- stack-and-queue/src/CountPrimes.java
+++ stack-and-queue/src/CountPrimes.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 /**
  * @Description: 204. 计数质数
  * 统计所有小于非负整数 n 的质数的数量。
@@ -11,6 +13,63 @@
  */
 public class CountPrimes {
 
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int c = in.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i <n; i++) {
+            arr[i] = in.nextInt();
+        }
+
+    }
+/*
+
+#include<bits/甩头dc++.h>
+using namespace std;
+const int N = 100200;
+int d[N],n,m;
+
+bool ch(int x){
+
+    int ct = m - 1;
+    int last = d[0];
+    for(int i = 1 ; i < n && ct; ++i){
+        while(i < n && d[i] - last < x) ++i;
+        if(i == n)  return false;
+        last = d[i];
+        --ct;
+    }
+    if(ct)
+        return false;
+    return true;
+}
+
+int main(){
+
+    cin >> n >> m;
+    for(int i = 0; i < n; ++i)
+        cin >> d[i];
+
+    sort(d, d + n);
+    int l = 0,r = 1e9 + 1,m,res = 0;
+
+    while(l <= r){
+        m = (l  + r) >> 1;
+        if(ch(m)){
+            res = max(res, m);
+            l = m + 1;
+        }else r = m - 1;
+    }
+
+    cout << res << '\n';
+    return 0;
+}
+
+
+
+
+ */
     // Eeatosthese筛法，每次除去非质数的整数倍
     public int countPrimes(int n) {
         if(n<=2){
